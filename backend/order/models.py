48fb5ca4e799795106919fa4517f08c64105eb91
Ilya <Bezzubik12345@i.ua>
2025-07-30 15:36:00 +0300

    Remove extra migration and unfinished models

--- backend/order/models.py
+++ backend/order/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from restaurant.models import Restaurant
 
 
-class Order(models.Model):
-    class Status(models.TextChoices):
-        PENDING = "pending", "Pending"
-        IN_PROGRESS = "in_progress", "In Progress"
-        COMPLETED = "completed", "Completed"
-        CANCELLED = "cancelled", "Cancelled"
+# class Order(models.Model):
+#     class Status(models.TextChoices):
+#         PENDING = "pending", "Pending"
+#         IN_PROGRESS = "in_progress", "In Progress"
+#         COMPLETED = "completed", "Completed"
+#         CANCELLED = "cancelled", "Cancelled"
